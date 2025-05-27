af9bd596da6883471041101b129acbb8af4bad74
kaustubhparab71 <33633535+kaustubhparab71@users.noreply.github.com>
2025-05-28 01:16:05 +0530

    Create kp07.py

---
+++ kp07.py
@@ -0,0 +1,10 @@
+def main():
+    name = "Kaustubh"
+    linkedin_url = "https://www.linkedin.com/in/vishram-parab-71kp/" 
+    # replace with your actual LinkedIn URL
+
+    print(f"Hi {name}, welcome to your LinkedIn page!")
+    print(f"🔗 Click here to view: {linkedin_url}")
+
+if __name__ == "__main__":
+    main()
